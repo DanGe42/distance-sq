c5a5f35d7a7b5fcd7fc66598d92b0fbc3db80b3a
Daniel Ge <dange@seas.upenn.edu>
2012-04-12 23:07:06 -0400

    Got the 4sq redirect working

--- distsq.py
+++ distsq.py
@@ -1,15 +1,29 @@
-from flask import Flask
+import foursquare
+from flask import Flask, render_template, url_for, redirect
 app = Flask(__name__)
 
 # configuration
 DEBUG = True
 SECRET_KEY = "development key"
+CLIENT_ID = "TUOPMCKP3D1LZBAUH2UOPOXCG4AXJ224NXZ54BOIWCK5KCI3"
+CLIENT_SECRET = "RDBMXGQNQNHAWK1YHZA04TPNYW3LB1UY401QCUPXPKHCOVEN"
 
 app.config.from_object(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/auth')
+def auth():
+    client = foursquare.Foursquare(client_id=CLIENT_ID, client_secret=CLIENT_SECRET,
+                                   redirect_uri="http://127.0.0.1:5000" + url_for('dashboard'))
+    auth_uri = client.oauth.auth_url()
+    return redirect(auth_uri)
+
+@app.route('/dashboard/')
+def dashboard():
+    pass
 
 if __name__ == '__main__':
     app.run()
