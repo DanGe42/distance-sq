45a988458347fef920d7fa8fe09150406dff5add
Daniel Ge <dange@seas.upenn.edu>
2012-04-28 22:13:16 -0400

    Added documentation and made the app slightly more robust. Also removed the old dashboard.html and renamed test3.html to replace it.

--- distsq.py
+++ distsq.py
@@ -1,4 +1,4 @@
-from foursquare import Foursquare, FoursquareException
+from foursquare import Foursquare, FoursquareException, InvalidAuth
 from flask import Flask, render_template, url_for, redirect, request, abort, \
                   session, flash
 import time
@@ -15,33 +15,56 @@ app.config.from_object(__name__)
 
 @app.route('/')
 def index():
+    """ Renders the index page. """
     return render_template('index.html')
 
 @app.route('/login')
 def login():
+    """ Called when the user clicks the login button. This function redirects to
+    the Foursquare OAuth page. """
     client = _get_foursquare()
     auth_uri = client.oauth.auth_url()
     return redirect(auth_uri)
 
 @app.route('/auth')
 def auth():
+    """ Callback for the Foursquare OAuth page. Foursquare gives us a 'code'
+    parameter on this call, so we parse this code and get the access token from
+    Foursquare. We store the access token into the session cookie.
+
+    If the user improperly calls this function, we redirect him back to the
+    index page with a error message. """
+
     code = request.args.get("code", None)
-    if code is not None:
+    try:
+        if code is None or code == "":
+            raise FoursquareException
+
         client = _get_foursquare()
         access_token = client.oauth.get_token(code)
-        session['access_token'] = access_token
-        return redirect(url_for('dashboard'))
-    else:
+    except FoursquareException:
         flash('Connect to your Foursquare account to log in')
         return redirect(url_for('index'))
+
+    session['access_token'] = access_token
+    return redirect(url_for('dashboard'))
 
 
 @app.route('/dashboard/')
 def dashboard():
+    """ Renders the dashboard. If the user is not logged in, we return the user
+    to the homepage with an error message.
+
+    The dashboard takes user-defined values of start and end times (defined in
+    /settings/). If the start and end times are not properly defined, we provide
+    default start (current time - 24 hours) and end times (current time). We
+    then query Foursquare for checkin data and render it onto dashboard.html.
+    """
+
     client = _get_foursquare()
     try:
         client.set_access_token(session['access_token'])
-    except KeyError:
+    except (KeyError, InvalidAuth):
         flash('You are not logged in.')
         return redirect(url_for('index'))
 
@@ -55,15 +78,16 @@ def dashboard():
     checkins = client.users.checkins(params=params)['checkins']
     checkins, locations, bounds, center = _process_checkins(checkins)
 
-    print "checkins = " + str(checkins)
-    print "locations = " + str(locations)
-
-    return render_template('test3.html', user=client.users()['user'],
+    return render_template('dashboard.html', user=client.users()['user'],
                            checkins=checkins,
                            locations= locations, bounds = bounds,
                            center=center, key=API_KEY, start=start, end=end)
 
 def _set_params(start, end):
+    """ Take the GET parameters provided to dashboard and puts them into the
+    params dictionary passed to the API call in dashboard(). See dashboard() for
+    how default values are calculated. """
+
     params = {}
     try:
         if not start:
@@ -105,6 +129,8 @@ def _process_checkins(checkins):
 
 @app.route('/settings/')
 def settings():
+    """ Allows the user to change the time range for checkins. If the user is
+    not logged in, we redirect him to the home page. """
     if 'access_token' not in session:
         flash('You are not logged in.')
         return redirect(url_for('index'))
@@ -112,12 +138,15 @@ def settings():
 
 @app.route('/logout')
 def logout():
+    """ Logs the user out by removing the access token from the session cookie.
+    """
     if session['access_token']:
         session.pop('access_token', None)
         flash('You have been logged out successfully')
     return redirect(url_for('index'))
 
 def _get_foursquare():
+    """ Returns the default Foursquare client object. """
     client = Foursquare(client_id=CLIENT_ID,
                         client_secret=CLIENT_SECRET,
                         redirect_uri="http://127.0.0.1:5000" +
@@ -144,14 +173,18 @@ def _list_locations(checkins):
     return result
 
 def _get_day_before(timestamp):
+    """ Returns (timestamp - 24 hours). The timestamp provided is Unix time in
+    seconds. """
     return timestamp - (24 * 60 * 60)
 
 def _bounds(dict_list):
-    """ Is meant to find the appropriate zoom value based on points on graph asssuming 72 DPI printout"""
+    """ Is meant to find the appropriate zoom value based on points on graph
+    asssuming 72 DPI printout"""
     max_lat = -90
     min_lat = 90
     max_long = -180
     min_long = 180
+
     for dict in dict_list:
         if dict['lat'] > max_lat:
             max_lat = dict['lat']
@@ -161,7 +194,11 @@ def _bounds(dict_list):
             max_long = dict['long']
         elif dict['long'] < max_long:
             min_long = dict['long']
-    return {'min_lat' : min_lat, 'min_long' : min_long, 'max_lat' : max_lat, 'max_long' : max_long}
+
+    return {'min_lat' : min_lat,
+            'min_long' : min_long,
+            'max_lat' : max_lat,
+            'max_long' : max_long}
 
 def _find_center(dict_list):
     """ Is meant to find the center of multiple points on a graph"""
