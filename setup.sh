04bb190a5883be34dc1e2770d1f623d57f519f5e
Daniel Ge <dange@seas.upenn.edu>
2012-04-29 00:04:16 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,8 +6,3 @@
 pip install Flask
 pip install poster
 pip install foursquare
-
-# getting pymaps.py (deprecated, using js instead)
-#svn checkout http://pymaps.googlecode.com/svn/trunk/ pymaps-read-only
-#mv pymaps-read-only/pymaps.py pymaps.py
-#rm -rf pymaps-read-only
